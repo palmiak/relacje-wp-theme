e64ec4bcf8ed5b1d0770e9175be8cfd0e3d51b73
Agnieszka Palmowska <a.palmowska@freshpixels.pl>
2024-12-16 19:24:45 +0100

    page tpl

--- page-template.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*
-Template Name: Page Template
-*/
-
-$context = Timber::get_context();
-
-Timber::render( 'views/templates/tpl-events-single.twig', $context );
---
+++ page.php
@@ -0,0 +1,5 @@
+<?php
+$context = Timber::get_context();
+$context['post'] = Timber::get_posts()[0];
+
+Timber::render( 'views/templates/page.twig', $context );
